eeb99f16a596cfc46582ef871566c84c00421b55
Sam Shacter <sshacter@gmail.com>
2024-06-16 19:01:43 -0500

    added the MUI Card to the details page to help style things a little better.

--- src/components/MovieItemDetails/MovieItemDetails.jsx
+++ src/components/MovieItemDetails/MovieItemDetails.jsx
@@ -2,7 +2,13 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import "./MovieItemDetails.css"
 
 
 
@@ -25,17 +31,32 @@ console.log(movieId.id)
   }
 
   return (
-    <div key={movieId} data-testid="movieDetails">
-        {console.log("movieDetails", movieDetails)}
-       <div><button data-testid="toList" onClick={backToList}>Back</button></div>
-        <div>{movieDetails.title}</div>
-        <div><img src={movieDetails.poster}/></div>
+    <div className="flex">
+    <Card raised="true" sx={{ maxWidth: 400 }} key={movieId} data-testid="movieDetails">
+       <CardMedia
+        component="img"
+        alt={movieDetails.title}
         
-        {movieDetails.genres.map((genre) => 
+        image={movieDetails.poster}
+      />
+      <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+          {movieDetails.title}
+        </Typography>
+        <Typography>
+
+            Genres: {movieDetails.genres.map((genre) => 
             <span key={genre.id}>{genre.name}</span>
         )}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {movieDetails.description}
+        </Typography>
         
-        <div>{movieDetails.description}</div>
+      </CardContent>
+        
+       <div><Button data-testid="toList" onClick={backToList}>Back</Button></div>
+     </Card>
      </div>
   );
 };
